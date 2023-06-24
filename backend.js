bd8881db71f9cac3d7fa7ba131bc694c461a6384
unknown <kolesnik.vitalik21@gmail.com>
2023-06-25 00:34:57 +0300

    get users data

--- backend.js
+++ backend.js
@@ -3,10 +3,19 @@ const path = require('path')
 const app = express()
 const PORT = 3000
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '\index.html'));
-  });
+let users = {
+    name: 'Vitaliy',
+    lastName: 'Kolesnik'
+}
 
+app.use('/api/users', function(req, res, next) {
+    res.json(users)
+    next()
+  })
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '\index.html'))
+  })
 
 app.listen(PORT, ()=>{
     console.log(`Express server is started on http://localhost:${PORT}`)
