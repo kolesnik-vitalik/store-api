470219eb4ab520ae3b49db5134102c88b4cd3228
unknown <kolesnik.vitalik21@gmail.com>
2023-06-25 11:16:21 +0300

    props output

--- backend.js
+++ backend.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = 3000
-const hostname = 'kolesnik-vitalik.github.io/store-api'
 
 let users = {
     name: 'Vitaliy',
@@ -11,13 +10,12 @@ let users = {
 
 app.use('/api/users', function(req, res, next) {
     res.json(users)
-    next()
   })
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '\index.html'))
   })
 
-app.listen(PORT, hostname, ()=>{
-    console.log(`Express server is started on http://${hostname}/${PORT}`)
+app.listen(PORT,()=>{
+    console.log(`Express server is started on http://localhost/${PORT}`)
 })
